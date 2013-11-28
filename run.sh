701cdc5a2bb0df82e09b2ec0e4d76db348d85dd1
Jonathan Doman <jonathan.doman@gmail.com>
2013-11-27 23:31:17 -0600

    run.sh now reads run.conf files for each benchmark

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-GPGPUSIM_DIR=/home/jonathan/gpgpu-sim/v3.x
-DATA_DIR="$(dirname $0)/../data"
-CUDA_DIR=/opt/cuda-4.0/cuda
+GPGPUSIM_DIR="/home/jonathan/gpgpu-sim/v3.x"
+RODINIA_DIR="/home/jonathan/rodinia"
+CUDA_DIR="/opt/cuda-4.2/cuda"
 
 # Check that benchmark exists
 if [[ ! -d "cuda/$1" ]]
@@ -12,6 +12,9 @@ then
 fi
 
 benchmark="$1"
+input="$3"
+source "cuda/$benchmark/run.conf"
+runCmd="$RODINIA_DIR/bin/linux/cuda/$runExec $runArgs"
 
 # Check for valid platforms
 platforms=(gt240 gtx460m gtx760 GTX480-sim QuadroFX5600-sim QuadroFX5800-sim TeslaC2050-sim)
@@ -32,18 +35,10 @@ then
    exit
 fi
 
-cd results
-
-if [[ ! -d "$benchmark" ]]
-then
-   mkdir $benchmark
-fi
-
-cd $benchmark
-
+_input=$input
 rundir="$platform"_$(date +%F-%H.%M)
-mkdir $rundir
-cd $rundir
+mkdir -p "results/$benchmark/$rundir"
+cd "results/$benchmark/$rundir"
 
 # Make sure data symlink is in place for the run script
 if [[ ! -L ../../data ]]
@@ -51,21 +46,19 @@ then
    ln -s ../../data ../../data
 fi
 
+echo -e "$runCmd\n$input\n" > log.txt
+
 if [[ "$platform" =~ (.*)-sim ]] 
 then
    # Set up the run directory
    base=${BASH_REMATCH[1]}
    simConfigDir="$GPGPUSIM_DIR/configs/$base"
    cp $simConfigDir/* ./
-   ln -s ../../../bin/linux/cuda/"$benchmark"
-   ln -s ../../../cuda/"$benchmark"/run
 
    # Run in gpgpu-sim
    export CUDA_INSTALL_PATH=$CUDA_DIR
    source $GPGPUSIM_DIR/setup_environment
-   cat run > log.txt
-   echo -e "\n\n" >> log.txt
-   ./run >> log.txt
+   eval $runCmd >> log.txt
 else
    gpus=$(nvidia-smi -L | sed -e 's/ //g' | tr '[:upper:]' '[:lower:]')
    if [[ "$gpus" != *"$platform"* ]]
@@ -76,10 +69,6 @@ else
       exit
    fi
 
-   ln -s ../../../bin/linux/cuda/"$benchmark"
-   ln -s ../../../cuda/"$benchmark"/run
    export LD_LIBRARY_PATH="$CUDA_DIR/lib64"
-   cat run > log.txt
-   echo -e "\n\n" >> log.txt
-   nvprof ./run >> log.txt
+   nvprof "$runCmd" >> log.txt
 fi

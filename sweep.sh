8fba558e76e51d709c677234589034ce3ef49a6c
Jonathan Doman <jonathan.doman@gmail.com>
2013-11-28 18:09:41 -0600

    Add sweep script to run all combinations
     - uses GNU parallel

---
+++ sweep.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+benchmarks=(backprop bfs b+tree cfd gaussian heartwall hotspot kmeans lavaMD lud mummergpu myocyte nn nw particlefilter pathfinder srad streamcluster)
+platforms=(hw GTX480-sim QuadroFX5600-sim QuadroFX5800-sim TeslaC2050-sim)
+
+cmdFile=$(mktemp)
+
+for b in ${benchmarks[@]}
+do
+   for p in ${platforms[@]}
+   do
+      input=""
+      if [[ $p == "hw" ]]
+      then
+         input="native"
+      else
+         input="small"
+      fi
+      echo "./run $b $p $input" >> $cmdFile
+   done
+done
+
+parallel -j6 < $cmdFile
+
+rm $cmdFile
